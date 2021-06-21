55a44acf635dc04ec7fdd1003d0594b56a6a8253
TienNguyen2 <tien.nguyen2@target.com>
2021-06-21 16:02:30 -0500

    try change

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import React, { useState } from 'react';
 
 import Game from './Game';
 
-
-
 const StarMatch = () => {
   const [gameId, setGameId] = useState(1);
   return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
